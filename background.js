8107dc172451033e38222da5d2c972b4ddaaa88a
Yash Desai <yddc902@live.com>
2020-04-24 23:23:01 +0530

    last update bug fixed

--- background.js
+++ background.js
@@ -14,15 +14,61 @@ chrome.runtime.onInstalled.addListener(() => {
 	setBadge() 
 })
 
-chrome.browserAction.onClicked.addListener(()=>{
-	setBadge() 
-})
+// chrome.browserAction.onClicked.addListener(()=>{
+// 	setBadge() 
+// })
+
+// window.onload = function() {
+// 	console.log("ckicked")
+//     setBadge()
+// };
 
 chrome.alarms.onAlarm.addListener((alarm) => {
 	setBadge()
 	console.log(alarm)
 })
 
+
+
+function checkForUpdate(){
+	setInterval(()=>{
+		$.ajax({
+			// url: 'update.json',
+			url:"https://gujarat-covid19-udate-api.herokuapp.com/update",
+			type: "GET",
+			dataType: 'json',
+			cors: true ,
+			contentType:'application/json',
+			secure: true,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+			success: function (load) {
+				//console.log(data)
+				chrome.storage.sync.get('checkUpdate', (data)=>{
+					console.log("got last_update: "+data.checkUpdate)
+					if(load===data.checkUpdate){
+						console.log("-----no changes----")
+					}
+					else{
+						console.log("----update found----")
+						chrome.storage.sync.set({
+							checkUpdate: load,
+						}, function() {
+							console.log("last_update set: "+load)
+							setBadge()
+						});
+					}
+				})
+				
+			},
+			error: function(XMLHttpRequest, textStatus, error) {
+				console.log(error)
+			}
+		})
+	}, 1000*60*60)
+}
+checkForUpdate()
 
 
 function setBadge(){
@@ -43,6 +89,7 @@ function setBadge(){
 		else if(parseInt(total)<=3000)
 			chrome.browserAction.setBadgeBackgroundColor({color: "#000000"})   
 		chrome.browserAction.setBadgeText({text: total});
+		console.log("badge updated")
 
 		},
 		error: function(XMLHttpRequest, textStatus, error) {
